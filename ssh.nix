7f7a6520d43dc021c697019b39bdb142f4d2658a
jeremyshaw <21230708+jeremyshaw@users.noreply.github.com>
2023-07-09 11:54:58 -0400

    For ssh, port 22/TCP is opened in firewall. Also minor whitespace change in a nixconfig.

--- ssh.nix
+++ ssh.nix
@@ -3,6 +3,7 @@
 {
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "no";
+  networking.firewall.allowedTCPPorts = [ 22 ];
   
   # does not work; this checks in root for the ssh key, not to mention, there is a chicken-egg problem with this on system bringup
   # programs.ssh.knownHosts = {
